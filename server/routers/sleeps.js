19643f5e8ec5176284452be551b3db5ab9e2b875
Cai Miller <Caimiller314@gmail.com>
2023-05-22 21:31:48 -0500

    updates

--- server/routers/sleeps.js
+++ server/routers/sleeps.js
@@ -15,4 +15,51 @@ router.post("/", (request, response) => {
   });
 });
 
+// Get (read) all records from the collection
+router.get("/", (request, response) => {
+  Sleep.find({}, (error, record) => {
+    if (error) return response.status(500).json(error.errors);
+    response.json(record);
+  });
+});
+
+// Get a single record by ID using a query parameter
+router.get("/:id", (request, response) => {
+  Sleep.findById(request.params.id, (error, record) => {
+    if (error) return response.status(500).json(error.errors);
+
+    response.json(record);
+  });
+});
+
+router.delete("/:id", (request, response) => {
+  Sleep.findByIdAndRemove(request.params.id, {}, (error, record) => {
+    if (error) return response.status(500).json(error.errors);
+    response.json(record);
+  });
+});
+
+router.put("/:id", (request, response) => {
+  const body = request.body;
+  Sleep.findByIdAndUpdate(
+    request.params.id,
+    {
+      $set: {
+        country: body.country,
+        state: body.state,
+        city: body.city
+      }
+    },
+    {
+      new: true,
+      upsert: true
+    },
+    (error, record) => {
+      if (error?.name === 'ValidationError') return response.status(400).json(error.errors);
+      if (error) return response.status(500).json(error.errors);
+      response.json(record);
+    }
+  );
+});
+
 module.exports = router;
